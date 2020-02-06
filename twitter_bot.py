f392485ae546ba746a485bc2e88f9fe15bcafd47
Dj <davidabraham@ucla.edu>
2020-02-06 09:48:52 -0800

    index got stuck in r/w process; changed order of operations

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,5 +1,5 @@
 # ****** Twitter Bot Module *****
-# AUTHOR: David James, 20200205
+# AUTHOR: David James, 20200206
 # Functions:
 # - function getCSV
 # - function getBlock
@@ -11,6 +11,7 @@
 from twython import Twython, TwythonError
 from csv import reader
 from collections import OrderedDict
+from time import sleep
 import boto3 as b3
 import os
 
@@ -134,15 +135,15 @@ def lambda_handler(event,context):
     text,geoid = getBlock(blist,index)
     print('GEOID:',geoid)
 
-    print('Uploading files')
-    s3.upload_file(TXT_PATH,BUCKET,TXT)
-    s3.upload_file(CSV_PATH,BUCKET,DATA)
-
     print('Downloading PNG')
     s3.download_file(BUCKET,KEY+geoid+'.png',IMG_PATH)
 
     print('Updating index')
     writeIndex(TXT_PATH,index)
+
+    print('Uploading files')
+    s3.upload_file(TXT_PATH,BUCKET,TXT)
+    s3.upload_file(CSV_PATH,BUCKET,DATA)
 
     print('Sending tweet out')
     twitterBot(text)
